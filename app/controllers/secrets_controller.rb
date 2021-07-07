98c097402a1e5b56646e7df51d9f97baaefb0bf9
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-07-07 09:28:51 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,28 @@
+class SecretsController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:index]
+
+
+    def new
+
+    end
+
+    def index
+
+   end
+
+    def show
+         @secret = Secret.find(params[:id])
+        
+
+    end
+    
+
+    private
+
+    def require_login
+    
+        return head(:forbidden) unless session.include? :user_id 
+
+    end
+end
